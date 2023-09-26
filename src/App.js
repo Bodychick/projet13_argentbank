e666c059ec21e670cbe5c4d104b0080df403cf0e
Alexandre Penneroux <101572526+Bodychick@users.noreply.github.com>
2023-09-26 18:22:24 +0200

    mise à j our du html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Accueil from './pages/accueil';
+import Connexion from './pages/connexion';
+import Profil from './pages/profil';
+import './pages/css/main.css'
+import Header from './composants/header';
+import Footer from './composants/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+            <Routes>
+                <Route path="/" element={<Accueil />} />
+                <Route path="/se-connecter" element={<Connexion />} />
+                <Route path="/profil" element={<Profil />} />
+            </Routes>
+        </Router>
+        <Footer />
     </div>
   );
 }
